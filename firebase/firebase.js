5ce4787e675efa1cd754827f8ac87f0603a3b619
Sashen Jayathilaka <99184393+SashenJayathilaka@users.noreply.github.com>
2022-11-10 07:59:48 +0530

    ✏️ edit environment variables(#24)
    edit next js environment variables

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDBtSO1ooUrDPYlZp2B_KZxi5E9Sy8cLKA",
-  authDomain: "tiktok-clone-be0aa.firebaseapp.com",
-  projectId: "tiktok-clone-be0aa",
-  storageBucket: "tiktok-clone-be0aa.appspot.com",
-  messagingSenderId: "668135954772",
-  appId: "1:668135954772:web:b442fc20cbefc5aa0450c1"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
